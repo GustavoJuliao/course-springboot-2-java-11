3278d77d5062ff972d7a1c79b72776f6f4d12942
GustavoSouza <gj.juliao>
2022-03-19 21:56:30 -0300

    Categories

---
+++ src/main/java/com/projectweb/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.projectweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectweb.course.entities.Category;
+import com.projectweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
