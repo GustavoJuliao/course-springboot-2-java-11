aeeadccd4722ea3932ec15a61337b4d3524fe944
GustavoSouza <gj.juliao>
2022-03-20 11:27:52 -0300

    Product-OrderItem one to many association

--- src/main/java/com/projectweb/course/entities/Order.java
+++ src/main/java/com/projectweb/course/entities/Order.java
@@ -35,7 +35,7 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 
@@ -84,11 +84,11 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
-	public Set<OrderItem> getItems(){
+
+	public Set<OrderItem> getItems() {
 		return items;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
